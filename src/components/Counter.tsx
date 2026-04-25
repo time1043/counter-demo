d69a468d1949ffa178a27fbcbe2d2614b1543e9f
oswin902m <1665434994@qq.com>
2026-04-25 11:11:43 +0800

    prevent unnecessary re-render from static selector function reference

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,7 @@
 import { useFlash } from "@/hooks/flash";
 import { useCounterStore } from "@/state/useCounterStore";
 import { useRef } from "react";
+import { useShallow } from "zustand/shallow";
 
 export function Counter() {
   return (
@@ -12,7 +13,9 @@ export function Counter() {
 }
 
 function CountDisplay() {
-  const { count } = useCounterStore();
+  // const { count } = useCounterStore();
+
+  const count = useCounterStore((state) => state.count);
 
   const cardRef = useRef<HTMLDivElement>(null);
   const renders = useFlash(cardRef);
@@ -26,7 +29,22 @@ function CountDisplay() {
 }
 
 function CountControls() {
-  const { increment, decrement, reset } = useCounterStore();
+  // const { increment, decrement, reset } = useCounterStore(); // return state
+
+  // // Only subscribes to actions / functions
+  // // Doesn't trigger re-renders, because the function reference don't change
+  // const increment = useCounterStore((state) => state.increment); // passing selector, which returns a function
+  // const decrement = useCounterStore((state) => state.decrement);
+  // const reset = useCounterStore((state) => state.reset);
+
+  // Though useShallow() checks keys, React doesn't re-render the component when the state changes (function reference doesn't change)
+  const { increment, decrement, reset } = useCounterStore(
+    useShallow((state) => ({
+      increment: state.increment,
+      decrement: state.decrement,
+      reset: state.reset,
+    })),
+  );
 
   const cardRef = useRef<HTMLDivElement>(null);
   const renders = useFlash(cardRef);
