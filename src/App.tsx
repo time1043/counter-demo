4917faecdda90651750054974d518bf56edabdc8
oswin902m <1665434994@qq.com>
2026-05-03 14:23:47 +0800

    jotai devtool

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import "jotai-devtools/styles.css";
 import { Counter } from "./components/Counter";
+import { DevTools } from "jotai-devtools";
+import css from "jotai-devtools/styles.css?inline";
+
+// https://github.com/jotaijs/jotai-devtools/discussions/188
+// https://github.com/jotaijs/jotai-devtools?tab=readme-ov-file#tree-shaking
+const JotaiDevTools = () =>
+  import.meta.env.MODE !== "production" ? (
+    <>
+      <style>{css}</style>
+      <DevTools />
+    </>
+  ) : null;
 
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <Counter />
+      <JotaiDevTools />
+    </>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import babel from "@rolldown/plugin-babel";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+// https://jotai.org/docs/tools/devtools
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      presets: ["jotai-babel/preset"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
